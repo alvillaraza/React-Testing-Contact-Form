1aca5c0047152108a315726401d29de5acefda29
alexis villaraza <alexis.borja@gmail.com>
2020-02-13 15:31:17 -0700

    imported mutationobserver

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,12 +10,27 @@ test("First Name, Last Name, Email, Message", () => {
   getByLabelText(/message/i);
 });
 
-test("form submit adds info to the list", () => {
-  const { getByLabelText, getByLabelText, getByTestId } = render(
-    <ContactForm />
-  );
-  const firstNameInput = getByLabelText(/firstName/i);
-  const lastNameInput = getByLabelText(/lastName/i);
-  const emailInput = getByLabelText(/email/i);
-  const messageInput = getByLabelText(/message/i);
-});
+// test("form submit adds info to the list", () => {
+//   const { getByLabelText, getByText } = render(
+//     <ContactForm />
+//   );
+//   const firstNameInput = getByLabelText(/firstName/i);
+//   const lastNameInput = getByLabelText(/lastName/i);
+//   const emailInput = getByLabelText(/email/i);
+//     const messageInput = getByLabelText(/message/i);
+    
+//     fireEvent.change(firstNameInput, { target: { value: 'Test firstName' } });
+//     fireEvent.change(lastNameInput, { target: { value: 'Test lastName' } });
+//     fireEvent.change(emailInput, { target: { value: 'Test email' } });
+//     fireEvent.change(messageInput, { target: { value: 'Test message' } });
+
+//     expect(firstNameInput.value).toBe('Test firstName');
+//     expect(lastNameInput.value).toBe('Test lastName');
+//     expect(emailInput.value).toBe('Test email');
+//     expect(messageInput.value).toBe('Test message');
+
+//     fireEvent.click(getByText(/submit!/i));
+
+    
+ 
+// });
