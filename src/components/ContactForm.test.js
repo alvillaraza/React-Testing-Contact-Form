1dabe04a433efe6b2d1761fa8394b53c638e47e6
alexis villaraza <alexis.borja@gmail.com>
2020-02-13 16:22:10 -0700

    got labels to pass

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,36 +1,31 @@
 import React from "react";
 import ContactForm from "./ContactForm";
-import { render } from "@testing-library/react";
+import { render, getByTestId, fireEvent } from "@testing-library/react";
 
-test("First Name, Last Name, Email, Message", () => {
+test("First Name, Last Name, Email, Message are being rendered", () => {
   const { getByLabelText } = render(<ContactForm />);
-  getByLabelText(/firstName/i);
-  getByLabelText(/lastName/i);
+  getByLabelText(/first Name/i);
+  getByLabelText(/last Name/i);
   getByLabelText(/email/i);
   getByLabelText(/message/i);
 });
 
-// test("form submit adds info to the list", () => {
-//   const { getByLabelText, getByText } = render(
-//     <ContactForm />
-//   );
-//   const firstNameInput = getByLabelText(/firstName/i);
-//   const lastNameInput = getByLabelText(/lastName/i);
-//   const emailInput = getByLabelText(/email/i);
-//     const messageInput = getByLabelText(/message/i);
-    
-//     fireEvent.change(firstNameInput, { target: { value: 'Test firstName' } });
-//     fireEvent.change(lastNameInput, { target: { value: 'Test lastName' } });
-//     fireEvent.change(emailInput, { target: { value: 'Test email' } });
-//     fireEvent.change(messageInput, { target: { value: 'Test message' } });
+test("form submit adds info to the list", () => {
+  const { getByLabelText, getByText } = render(<ContactForm />);
+  const firstNameInput = getByLabelText(/first Name/i);
+  const lastNameInput = getByLabelText(/last Name/i);
+  const emailInput = getByLabelText(/email/i);
+  const messageInput = getByLabelText(/message/i);
 
-//     expect(firstNameInput.value).toBe('Test firstName');
-//     expect(lastNameInput.value).toBe('Test lastName');
-//     expect(emailInput.value).toBe('Test email');
-//     expect(messageInput.value).toBe('Test message');
+  fireEvent.change(firstNameInput, { target: { value: "Test firstName" } });
+  fireEvent.change(lastNameInput, { target: { value: "Test lastName" } });
+  fireEvent.change(emailInput, { target: { value: "Test email" } });
+  fireEvent.change(messageInput, { target: { value: "Test message" } });
 
-//     fireEvent.click(getByText(/submit!/i));
+  expect(firstNameInput.value).toBe("Test firstName");
+  expect(lastNameInput.value).toBe("Test lastName");
+  expect(emailInput.value).toBe("Test email");
+  expect(messageInput.value).toBe("Test message");
 
-    
- 
-// });
+  fireEvent.click(getByText(/submit/i));
+});
